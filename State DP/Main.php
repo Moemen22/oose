840e6c419cbfdd6df9f2cf7eb5936556a73ae1bc
Joehamdy2 <93213421+Joehamdy2@users.noreply.github.com>
2022-01-24 18:46:08 +0200

    Update Main.php

--- State DP/Main.php
+++ State DP/Main.php
@@ -3,12 +3,16 @@ include_once('State.php');
 include_once('StartState.php');
 include_once('StopState.php');
 include_once('Context.php');
+include_once('checkedman.php');
 $context = new Context();
 $startState = new StartState();
 $startState->doAction($context);
 print_r ($context->getState());
-
+$Checkedman = new checkedman();
+$Checkedman->doAction($context);
+print_r($context->getState());
 $stops = new StopState();
 $stops->doAction($context);
 print_r($context->getState());
+
 ?>
